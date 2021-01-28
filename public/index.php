8d3a3e8fc144576802cb88f34104f357600a02d7
x4lva <30348460+Dimonchak@users.noreply.github.com>
2021-01-28 16:14:55 +0200

    Basic styles, register form validation

--- public/index.php
+++ public/index.php
@@ -20,3 +20,4 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -18,9 +18,9 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, array(
+            ->add('username', TextType::class, [
                 'label' => 'Enter username'
-            ))
+            ])
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
                 'first_options' => array(
